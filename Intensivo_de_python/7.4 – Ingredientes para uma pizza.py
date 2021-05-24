25220f71d0f14f38b2bb9939cf44a30bd8e4e086
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-05-24 16:58:12 -0300

    atualizando exercicios

--- Intensivo_de_python/7.4 – Ingredientes para uma pizza.py
+++ Intensivo_de_python/7.4 – Ingredientes para uma pizza.py
@@ -3,16 +3,19 @@ cont = 0
 while True:
     ingrediente = str(input('Digite os ingrediente da sua pizza: ')).lower()
     print(f'Irei adicionar o {cont + 1}ª ingrediente {ingrediente} na sua pizza')
-    print('Digite Exit caso não queira mais adicionar mais ingrediente')
+    print('Digite Exit caso não queira mais adicionar mais ingrediente\n')
     pizza.append(ingrediente)
     cont += 1
     if ingrediente == 'exit':
-        print('Muito obrigado sua pizza esta sendo levada para o forno:')
+        print('Muito obrigado sua pizza esta sendo levada para o forno:\n')
+        break
     elif cont > 10:
         cont = cont - 1
-        print('Muito obrigado, Porem voce ja atingiu o limite maximo de ingredientes das sua pizza, digite Exit para encerrar o programa, que loga estaremos levando sua pizza para o forno')
+        print('Muito obrigado, Porem voce ja atingiu o limite maximo de ingredientes das sua '
+              'pizza, digite Exit para encerrar o programa, que loga estaremos levando sua pizza para o forno\n')
+        break
 
 
 print('Estou feliz que tenha escolhido seus ingredientes')
 print(f'O ingredientes da sua pizza foram {pizza}')
-print('Obrigado pela preferencia')    
+print('Obrigado pela preferencia')
