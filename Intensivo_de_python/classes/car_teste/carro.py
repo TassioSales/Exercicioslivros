be80f01cffdc1274fbd5785354980fee06ce56df
TassioSales <tassio.ljs@gmail.com>
2021-08-17 00:18:44 -0300

    commit inicial

---
+++ Intensivo_de_python/classes/car_teste/carro.py
@@ -0,0 +1,32 @@
+class Carro():
+    """Uma tentativa simples de representar uma carro"""
+    def __init__(self, make, model, year): #Inicializa os atributos que descrevem  um carro;
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odemeter_reading = 0  
+        
+    def get_descriptive_name(self):
+        """Devolve um nome descritivo, formatado de modo elegante"""
+        long_name = ("{} {} {}".format(str(self.year), self.make, self.model)) 
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Exibe uma frase que mostra a milhagem do carro."""
+        print("This cas has {} miles on it".format(str(self.odemeter_reading)))
+        
+    def update_odemeter(self, milage ):
+        """Define o valor de leitura do hodometro com valor especificado"""
+        self.odemeter_reading = milage
+    
+if __name__ == "__main__":
+    
+    carro1 = Carro('audi', 'a4', 2016)
+    carro2 = Carro("Ford", "Palio", 2020)
+    
+    print(Carro.get_descriptive_name(carro1))
+    carro1.update_odemeter(24)
+    print(Carro.read_odometer(carro1))
+    print(Carro.get_descriptive_name(carro2))
+    carro2.update_odemeter(31)
+    print(Carro.read_odometer(carro2))
